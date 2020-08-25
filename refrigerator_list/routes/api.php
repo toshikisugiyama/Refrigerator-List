d5530b4d7dad617120321dcc692a7ea09c55fb70
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-08-25 13:35:51 +0900

    install laravel/sanctum

--- refrigerator_list/routes/api.php
+++ refrigerator_list/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
