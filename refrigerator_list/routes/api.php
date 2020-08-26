f9c51c15c2f3926e2611609e8c7c63076f142ef8
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-08-26 15:16:38 +0900

    add login and register controllers

--- refrigerator_list/routes/api.php
+++ refrigerator_list/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('user', function (Request $request) {
+        return response()->json(['user' => $request->user()]);
+    });
+    Route::post('logout', 'Auth\Api\LoginController@logout')->name('api.logout');
+    Route::apiResources([
+        'refrigerator' => 'RefrigeratorController',
+        'item' => 'ItemController'
+    ]);
 });
-
-Route::apiResources([
-    'refrigerator' => 'RefrigeratorController',
-    'item' => 'ItemController'
-]);
+Route::post('register', 'Auth\Api\RegisterController@register')->name('api.register');
+Route::post('login', 'Auth\Api\LoginController@login')->name('api.login');
